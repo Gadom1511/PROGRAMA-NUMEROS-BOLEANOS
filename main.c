5bc318b56211d96a6b53430b3e3a3d087814a53f
Gadom1511 <marloncajamarca72@gmail.com>
2026-01-26 15:50:16 -0500

    feat: :sparkles: modificacion para el menu

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 void main (){
-    int verdadero=1 , falso =0,num_variables,op;
+    int verdadero=1 , falso =0,num_variables,op,exponente;
     
+    printf("Ingrese el numero de variables: ");
+    scanf("%d",&num_variables); 
+    printf("Ingrese la opcion (1 para SOP, 2 para POS): ");
+    scanf("%d",&op);
     switch(op){
         case 1:
-            printf("Usted selecciono la Opcion SOP);
+            printf("Usted selecciono la Opcion SOP");
+
             break;
         case 2:
             printf("Usted selecciono la Opcion POS");
