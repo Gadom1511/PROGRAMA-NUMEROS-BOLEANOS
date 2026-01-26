ce75f6de1c72c81b2b65fed47f2da1539ae49101
Gadom1511 <marloncajamarca72@gmail.com>
2026-01-26 15:13:56 -0500

    feat: :sparkles: realizacion del menu

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main (){
+    int verdadero=1 , falso =0,num_variables,op;
+    
+    switch(op){
+        case 1:
+            printf("Usted selecciono la Opcion SOP);
+            break;
+        case 2:
+            printf("Usted selecciono la Opcion POS");
+            break;
+        default:
+            printf("Opcion no valida\n");
+    }
+            
+}
