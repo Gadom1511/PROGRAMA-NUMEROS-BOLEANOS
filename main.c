1417fdb468f48a6a6d4dd9f28fddbf05af4b37bd
Gadom1511 <marloncajamarca72@gmail.com>
2026-01-27 17:11:55 -0500

    fix: :bug:

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 
 
 void main (){
-    int num_variables,op,exponente,base=2,salida;
+    int num_variables,op,exponente,base=2,salida,i,j;
     char letra;
     printf("Ingrese el numero de variables: ");
     scanf("%d",&num_variables); 
     exponente = pow(base,num_variables);
     printf("El numero de filas es: %d\n",exponente);
-    printf("Ingrese la opcion (1 para SOP, 2 para POS): ");
-    scanf("%d",&op);
     do
     {
+     printf("Ingrese la opcion (1 para SOP, 2 para POS): ");
+     scanf("%d",&op);
         switch(op){
         case 1:
             printf("Usted selecciono la Opcion SOP");
-            
+
             break;
         case 2:
             printf("Usted selecciono la Opcion POS");
@@ -25,7 +25,6 @@ void main (){
         default:
             printf("Opcion no valida\n");
     }
-    } while (op<1 || op>2);
-    
+    } while (op!=1 && op!=2);
             
 }
